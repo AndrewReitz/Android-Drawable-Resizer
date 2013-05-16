6324dddb842168aa05224bc27c5bf8077ab93ab5
Pieces <deadhead1337@yahoo.com>
2013-05-15 23:59:12 -0500

    Added license to files

---
+++ js/app/ready.js
@@ -0,0 +1,54 @@
+/*
+ *   This file is part of Android-Drawable-Resizer.
+ *
+ *   Android-Drawable-Resizer is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Android-Drawable-Resizer is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Android-Drawable-Resizer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+define(function () {
+    'use strict';
+
+    /*
+     * Function called when the dom loads from DOMContenetLoaded
+     */
+    function ready() {
+        alert('hello');
+    }
+
+    /*
+     * Unbind the dom load event listener and fire the ready event
+     */
+    var DOMContentLoaded = function () {
+        if (document.addEventListener) {
+            document.removeEventListener('DOMContentLoaded', DOMContentLoaded, false);
+            ready();
+        } else if (document.readyState === "complete") {
+            window.document.detachEvent('onreadystatechange', DOMContentLoaded);
+            ready();
+        }
+    };
+
+    /*
+     * Add event listeners for when the dom loads fire DOMContenetLoaded
+     * when it does (also checks if DOM is already loaded if it has been just fire the ready function)
+     */
+    if (document.readyState === 'complete') {
+        setTimeout(ready, 1);
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', DOMContentLoaded, false);
+        window.addEventListener("load", ready, false);
+    } else { //ie..
+        document.attachEvent('onreadysteatechange', DOMContentLoaded);
+        window.attachEvent("onload", ready);
+    }
+});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
+/*
+ *   This file is part of Android-Drawable-Resizer.
+ *
+ *   Android-Drawable-Resizer is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Android-Drawable-Resizer is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Android-Drawable-Resizer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 require.config({
     paths: {
-        "jszip": "lib/jszip/jszip"
+        jszip: "lib/jszip/jszip"
     }
+});
+
+// Start the main app logic.
+requirejs(['app/ready'], function() {
+    alert('hello');
 });
