cf1a81e1f594e39c7ccfbc7ae0c5f0c491c2a525
Pieces <deadhead1337@yahoo.com>
2013-06-18 23:59:51 -0500

    Created densities and imageLoader

---
+++ src/js/app/Densities.js
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Android-Drawable-Resizer.
+ *
+ * Android-Drawable-Resizer is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ *
+ */
+
+define(function(){
+    'use strict';
+
+    var Densities = {
+        XHDPI: 1,
+        HDPI: 2,
+        MDPI: 3
+    };
+
+    Object.freeze(Densities);
+    return Densities;
+});
