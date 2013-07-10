78a1e41c87041a14738098660463d01608bd4e38
Pieces <deadhead1337@yahoo.com>
2013-07-09 23:00:51 -0500

    added compilation for requirejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,14 @@
+/*
+ * This file is part of Android-Drawable-Resizer.
+ *
+ * Android-Drawable-Resizer is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ *
+ */
+
 module.exports = function (grunt) {
     'use strict';
 
@@ -31,11 +42,24 @@ module.exports = function (grunt) {
                     jshintrc: 'test/js/.jshintrc'
                 }
             }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: 'src/js/',
+                    paths: {
+                        jszip: "lib/jszip"
+                    },
+                    name: "main",
+                    out: 'optimized.js'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
 
     // Default task
     grunt.registerTask('default', ['jshint']);
