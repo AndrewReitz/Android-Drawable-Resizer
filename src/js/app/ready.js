3d8a01ef5622c18019cc4f46cbfa8c6ae5e661eb
Pieces <deadhead1337@yahoo.com>
2013-07-09 22:26:22 -0500

    updated how require is used

--- src/js/app/ready.js
+++ src/js/app/ready.js
@@ -9,53 +9,54 @@
  *
  */
 
-define(function (require) {
-    'use strict';
+define([
+    'app/inputFileHandler',
+    'app/fileLoader',
+    'app/fileFilter',
+    'app/imageLoader',
+    'app/fileZipper'],
+    function (InputFileHandler, FileLoader, FileFilter, ImageLoader, FileZipper) {
+        'use strict';
 
-    var InputFileHandler = require('app/inputFileHandler');
-    var FileLoader = require('app/fileLoader');
-    var FileFilter = require('app/fileFilter');
-    var ImageLoader = require('app/imageLoader');
-    var FileZipper = require('app/fileZipper');
+        /**
+         * Function called when the dom loads from DOMContenetLoaded
+         * Place code here
+         */
+        function ready() {
+            var inputFilesElement = document.getElementById("inputFiles");
 
-    /**
-     * Function called when the dom loads from DOMContenetLoaded
-     * Place code here
-     */
-    function ready() {
-        var inputFilesElement = document.getElementById("inputFiles");
-
-        var fileZipper = new FileZipper();
-        var imageLoader = new ImageLoader(fileZipper.zip.bind(fileZipper));
-        var fileLoader = new FileLoader(imageLoader.loadImage.bind(imageLoader));
-        var fileFilter = new FileFilter(/^image\//, fileLoader.loadFile.bind(fileLoader));
-        new InputFileHandler(inputFilesElement, fileFilter.checkFile.bind(fileFilter), imageLoader);
-    }
-
-    /**
-     * Unbind the dom load event listener and fire the ready event
-     */
-    var DOMContentLoaded = function () {
-        if (document.addEventListener) {
-            document.removeEventListener('DOMContentLoaded', DOMContentLoaded, false);
-            ready();
-        } else if (document.readyState === "complete") {
-            window.document.detachEvent('onreadystatechange', DOMContentLoaded);
-            ready();
+            var fileZipper = new FileZipper();
+            var imageLoader = new ImageLoader(fileZipper.zip.bind(fileZipper));
+            var fileLoader = new FileLoader(imageLoader.loadImage.bind(imageLoader));
+            var fileFilter = new FileFilter(/^image\//, fileLoader.loadFile.bind(fileLoader));
+            new InputFileHandler(inputFilesElement, fileFilter.checkFile.bind(fileFilter),
+                imageLoader);
         }
-    };
 
-    /**
-     * Add event listeners for when the dom loads fire DOMContenetLoaded
-     * when it does (also checks if DOM is already loaded if it has been just fire the ready function)
-     */
-    if (document.readyState === 'complete') {
-        setTimeout(ready, 1);
-    } else if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', DOMContentLoaded, false);
-        window.addEventListener("load", ready, false);
-    } else { //ie..
-        document.attachEvent('onreadysteatechange', DOMContentLoaded);
-        window.attachEvent("onload", ready);
-    }
-});
+        /**
+         * Unbind the dom load event listener and fire the ready event
+         */
+        var DOMContentLoaded = function () {
+            if (document.addEventListener) {
+                document.removeEventListener('DOMContentLoaded', DOMContentLoaded, false);
+                ready();
+            } else if (document.readyState === "complete") {
+                window.document.detachEvent('onreadystatechange', DOMContentLoaded);
+                ready();
+            }
+        };
+
+        /**
+         * Add event listeners for when the dom loads fire DOMContenetLoaded
+         * when it does (also checks if DOM is already loaded if it has been just fire the ready function)
+         */
+        if (document.readyState === 'complete') {
+            setTimeout(ready, 1);
+        } else if (document.addEventListener) {
+            document.addEventListener('DOMContentLoaded', DOMContentLoaded, false);
+            window.addEventListener("load", ready, false);
+        } else { //ie..
+            document.attachEvent('onreadysteatechange', DOMContentLoaded);
+            window.attachEvent("onload", ready);
+        }
+    });
